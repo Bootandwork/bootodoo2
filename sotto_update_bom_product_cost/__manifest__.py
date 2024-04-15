8ace86503c95e70fc4ae0417626389e184b78c55
Dídac Alsina | IPGrup <93338796+dalsina@users.noreply.github.com>
2024-04-15 09:26:22 +0200

    [INS] function to iterate and compute from bom

---
+++ sotto_update_bom_product_cost/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'Auto Update BOM Product Cost',
+    "author": "Sottosoft Solution",
+    'version': '16.0.1',
+    "website": "",
+    'category': 'Extra Tools',
+    "support": "sottosoftsolution@gmail.com",
+    'summary': 'Auto Update BOM Product Cost,Auto Update Cost,Update cost,Update Product Cost,update cost from bom',
+    'description': """
+        Update BOM Product Cost automatically when a bill of material is created or updated. Auto Update BOM Product Cost,Auto Update Cost,Update cost,Update Product Cost,update cost from bom
+    """,
+    'images': ["static/description/img_1.png"],
+    'depends': ['base_setup','sale_margin','mrp'],
+    'data': [],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+    'application':True,
+    "price": 0,
+    "currency": "EUR"
+}   
+
---
+++ sotto_update_bom_product_cost/models/mrp_bom.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+from odoo import fields, models, api
+
+class MrpBom(models.Model):
+    _inherit="mrp.bom"
+
+    def update_bom_product_cost(self):
+        for rec in self:
+            temp_list=[]
+            if rec.bom_line_ids:
+                for first_bom in rec.bom_line_ids:
+                    temp_list.append(first_bom.product_tmpl_id)
+                for record in temp_list:
+                    bom=self.env['mrp.bom'].search([('product_tmpl_id','=',record.id)],limit=1)
+                    if bom.bom_line_ids:
+                        for record in bom.bom_line_ids:
+                            temp_list.append(record.product_tmpl_id)
+            if temp_list:
+                for cost_update in reversed(temp_list):
+                    cost_update.button_bom_cost()
+            rec.product_tmpl_id.button_bom_cost()
+            if rec.product_id:
+                rec.product_id.button_bom_cost()
+
+    """
+    @api.model
+    def create(self,vals):
+        res = super(MrpBom,self).create(vals)
+        res.update_bom_product_cost()
+        domain = [('product_id', '=', res.product_id.id),('order_id.state', '=', 'draft')]
+        find_sol = self.env['sale.order.line'].search(domain)
+        for data in find_sol:
+            data._compute_purchase_price()
+        return res
+
+    def write(self,vals):
+        res = super(MrpBom,self).write(vals)
+        for rec in self:
+            rec.update_bom_product_cost()
+            domain = [('product_id', '=', rec.product_id.id),('order_id.state', '=', 'draft')]
+            find_sol = self.env['sale.order.line'].search(domain)
+            for data in find_sol:
+                data._compute_purchase_price()
+        return res
+    """
+    
