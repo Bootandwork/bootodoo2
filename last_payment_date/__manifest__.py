7d2e67ba7f26e298887e18a2704f5d6a0eb438b1
SERGIO-IPGRUP <speralta@ipgrup.cat>
2024-06-07 09:44:24 +0200

    [ADD] last_payment_date

---
+++ last_payment_date/__manifest__.py
@@ -0,0 +1,14 @@
+# invoice_last_payment_date/__manifest__.py
+{
+    'name': 'Invoice Last Payment Date',
+    'version': '1.0',
+    'summary': 'Adds a field to store the date of the last payment on invoices',
+    'author': 'Tu Nombre',
+    'category': 'Accounting',
+    'depends': ['account'],
+    'data': [
+        'views/account_move_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
