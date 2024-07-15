5de368f25eb872878b2963d0a842303da8dbb600
SERGIO-IPGRUP <speralta@ipgrup.cat>
2024-07-15 16:38:55 +0200

    [UPD] Use record instead of self

--- last_payment_date/models/account_move.py
+++ last_payment_date/models/account_move.py
@@ -11,13 +11,13 @@ class AccountMove(models.Model):
         # self.ensure_one()
         for record in self:
             payments = self.env['account.payment'].search([
-                ('ref', '=', self.name)
+                ('ref', '=', record.name)
             ], order='date desc', limit=1)
             if payments:
                 record.last_payment_date = payments.date
             else:
                 payments = self.env['payment.transaction'].search([
-                ('invoice_ids', '=', self.id)
+                ('invoice_ids', '=', record.id)
             ], order='last_state_change desc', limit=1)
                 if payments:
                     record.last_payment_date = payments.last_state_change.date()
