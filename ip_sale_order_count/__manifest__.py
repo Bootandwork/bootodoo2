f778dca12384dc7d17e2f01281751d1a2dcc80c4
Gerard-IPGRUP <ggonzalez@ipgrup.com>
2024-07-29 15:48:58 +0200

    [UPD] Manifest Changes

--- ip_sale_order_count/__manifest__.py
+++ ip_sale_order_count/__manifest__.py
@@ -2,14 +2,15 @@
 {
     'name': 'Sale Order Count',
     'version': '16.0.0.0.0',
-    'description': "Custom module for MontexFR",
+    'description': "Confirmed Sale Order Count",
     'author': 'IPGrup',
     'company': 'IPGrup',
     'maintainer': 'Gerard - ggonzalez@ipgrup.com',
     'website': 'https://www.ipgrup.com',
     'depends': ['contacts', 'sale'],
     'data': [
-        'views/res_partner_views.xml',
+        'views/res_partner.xml',
+        'views/sale_order.xml',
     ],
     'installable': True,
     'auto_install': False,
