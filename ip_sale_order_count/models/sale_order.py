510107b85dd2269754f3dce09b02e1dea30f0c6b
Gerard-IPGRUP <ggonzalez@ipgrup.com>
2024-07-29 16:19:37 +0200

    [UPD] Sale Order Count

--- ip_sale_order_count/models/sale_order.py
+++ ip_sale_order_count/models/sale_order.py
@@ -3,4 +3,4 @@ from odoo import models, fields, api
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    ip_sale_order_count = fields.Integer(string="Pedidos de Venta", related="partner_id.ip_sale_order_count")
+    ip_sale_order_count = fields.Integer(string="Sale Order Count", related="partner_id.ip_sale_order_count")
