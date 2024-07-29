8de2489c6b051ccac25a08d73e15eda5709e1d6e
Gerard-IPGRUP <ggonzalez@ipgrup.com>
2024-07-29 16:09:26 +0200

    [UPD] Field name + xpath

--- ip_sale_order_count/models/res_partner.py
+++ ip_sale_order_count/models/res_partner.py
@@ -3,7 +3,7 @@ from odoo import models, fields, api
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
-    ip_sale_order_count = fields.Integer(string="Pedidos de Venta", compute="_compute_ip_sale_order_count")
+    ip_sale_order_count = fields.Integer(string="Sale Order Count", compute="_compute_ip_sale_order_count")
 
     @api.depends('sale_order_ids.state')
     def _compute_ip_sale_order_count(self):
