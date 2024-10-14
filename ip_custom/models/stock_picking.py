fabd00fbb4b04a76fccdb95a1f516986f1e98586
SERGIO-IPGRUP <speralta@ipgrup.cat>
2024-10-14 14:54:12 +0200

    test

--- ip_custom/models/stock_picking.py
+++ ip_custom/models/stock_picking.py
@@ -6,7 +6,7 @@ class StockPicking(models.Model):
 
     ip_validate_control = fields.Boolean(string = "Validate Control", compute="_compute_ip_validate_control", store=True)
 
-    @api.depends('picking_type_id.code', 'sale_id')
+    @api.depends('picking_type_id.code', 'sale_id', 'sale_id.x_studio_shipping_allowd')
     def _compute_ip_validate_control(self):
         for rec in self:
             # Default the control to False
@@ -17,11 +17,13 @@ class StockPicking(models.Model):
                 # If x_studio_shipping_allowd is True, set control to True
                 if rec.sale_id.x_studio_shipping_allowd:
                     rec.ip_validate_control = True
+                else:
+                    rec.ip_validate_control = False
                 # Otherwise, check if picking type is 'outgoing'
-                elif rec.picking_type_id.code == "outgoing":
-                    rec.ip_validate_control = True
-                # If both are False, leave it as False (default)
             else:
                 # If no sale order, check if picking type is 'outgoing'
                 if rec.picking_type_id.code == "outgoing":
                     rec.ip_validate_control = True
+
+                else:
+                    rec.ip_validate_control = False
