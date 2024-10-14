84ea9e00d6ba1434b29236783cb74ec404d1d580
SERGIO-IPGRUP <speralta@ipgrup.cat>
2024-10-14 12:04:53 +0200

    [ADD] IP Custom

---
+++ ip_custom/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'IP Custom',
+    'version': '16.0.0.0.0',
+    'description': "IP Custom",
+    'author': 'IPGrup',
+    'company': 'IPGrup',
+    'maintainer': 'Gerard - ggonzalez@ipgrup.com',
+    'website': 'https://www.ipgrup.com',
+    'depends': ['stock', 'sale'],
+    'data': [
+        'views/stock_picking_views.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
---
+++ ip_custom/models/stock_picking.py
@@ -0,0 +1,19 @@
+from odoo import api, fields, models, _
+
+class StockPicking(models.Model):
+
+    _inherit = "stock.picking"
+
+    ip_validate_control = fields.Boolean(string = "Validate Control", compute="_compute_ip_validate_control")
+
+    @api.depends('sale_id','picking_type_id.code','state')
+    def _compute_ip_validate_control(self):
+        for rec in self:
+            if rec.sale_id:
+                if rec.sale_id.x_studio_shipping_allowd:
+                    rec.ip_validate_control = True
+                else:
+                    if rec.picking_type_id.code == "outgoing":
+                        rec.ip_validate_control = True
+                    else:
+                        rec.ip_validate_control = False
